78846a2b1f3e79ad3d2d96ca2134878c993f9f68
danrlei-oliveira <danrlei.jesus@hotmail.com>
2023-11-02 10:11:09 -0300

    comentando o codigo

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+// criando um modulo AngularJS
 var app = angular.module('catalogApp', []);
 
+// definindo o controller
 app.controller('catalogController', function($scope) {
+    // criando um array de objetos que representa os produtos no catálogo
     $scope.products = [
         {
             name: 'Produto 1',
@@ -19,8 +22,10 @@ app.controller('catalogController', function($scope) {
         },
     ];
 
+    // inicializa uma variável como nula
     $scope.selectedProduct = null;
 
+    // função que é chamada quando um produto é clicado para mostrar seus detalhes
     $scope.showProductDetails = function(product) {
         $scope.selectedProduct = product;
     };
