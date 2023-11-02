20b5019bd8faf5c18d8737b219bf0301a3ee92e6
danrlei-oliveira <danrlei.jesus@hotmail.com>
2023-11-02 10:47:37 -0300

    arrumando os bugs nos codigos

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-// criando um modulo AngularJS
-var app = angular.module('catalogApp', []);
+// criando o módulo AngularJS
+var app = angular.module("catalogApp", []);
 
 // definindo o controller
-app.controller('catalogController', function($scope) {
-    // criando um array de objetos que representa os produtos no catálogo
+app.controller("CatalogController", function($scope) {
+    // criando o array de objetos que representa os produtos no catálogo
     $scope.products = [
         {
-            name: 'Produto 1',
+            name: "Produto 1",
             price: 19.99,
-            description: 'Descrição do Produto 1',
+            description: "Descrição do Produto 1"
         },
         {
-            name: 'Produto 2',
+            name: "Produto 2",
             price: 29.99,
-            description: 'Descrição do Produto 2',
+            description: "Descrição do Produto 2"
         },
         {
-            name: 'Produto 3',
+            name: "Produto 3",
             price: 39.99,
-            description: 'Descrição do Produto 3',
-        },
+            description: "Descrição do Produto 3"
+        }
     ];
 
-    // inicializa uma variável como nula
+    // inicializando uma variável como nula
     $scope.selectedProduct = null;
 
     // função que é chamada quando um produto é clicado para mostrar seus detalhes
     $scope.showProductDetails = function(product) {
         $scope.selectedProduct = product;
-    };
+    }
 });
